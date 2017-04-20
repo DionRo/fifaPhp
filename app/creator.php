dd9146e152b46aa5acb63358af640de33256bc9f
Unknown <d223013@edu.rocwb.nl>
2017-04-20 12:18:29 +0200

    patch laatste van vandaag

--- app/creator.php
+++ app/creator.php
@@ -17,34 +17,6 @@ require('database.php');
       if (empty($_POST['prijs'])){header("Location:  ../public/create.php?message=$ErrorMessage");die;}
 
 
-
-$target_dir = "img/";
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-
-    $ErrorMessage = "<strong>Het bestand is te groot! Probeer een kleinere afbeelding</strong>";
-    if ($_FILES["fileToUpload"]["size"] > 500000)
-        {header("Location: ../public/create.php?message=$ErrorMessage"); $uploadOk = 0;die;}
-
-    $ErrorMessage = "<strong>Sorry, alleen JPG, JPEG, PNG en GIF files zijn toegestaan</strong>";
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif" )
-        {header("Location: ../public/create.php?message=$ErrorMessage"); $uploadOk = 0;die;}
-    $ErrorMessage = "<strong>Er is iets fout gegaan met het uploaden van de afbeelding! Probeer het later nog eens!</strong>";
-    if ($uploadOk == 0)
-    {
-        header("Location: ../public/create.php?message=$ErrorMessage");
-        $uploadOk = 0;
-    }
-    else {
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-        } else {
-            echo "Sorry, there was an error uploading your file.";
-        }
-    }
-
       $sqlAdd = "INSERT INTO " .$dbname . "." . $table . "(`titel`, `beschrijving`,`prijs`,`categorie_id`,`foto`) VALUES ('" . $titel . "','" . $omschrijving . "','" . $prijs . "',
       '" . $categorie . "','" . $target_file . "')";
       $sqlAddObj = $db_conn->prepare($sqlAdd);
--- app/database.php
+++ app/database.php
@@ -2,7 +2,12 @@
 
 
 $dbname  = "project_fifa";
-$table  = "producten";
+$table   = "tbl_teams";
+$table2  = "tbl_users";
+$table3  = "tbl_poules";
+$table4  = "tbl_players";
+$table5  = "tbl_matches";
+
 $db_conn = new PDO('mysql:host=localhost;dbname=project_fifa','root','');
 
 $db_conn->setAttribute(PDO::ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
