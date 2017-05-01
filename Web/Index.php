558eb8448229588acb97b41e97d9237faa15208c
Kutay Yalçinkaya <kutay_yal@outlook.com>
2017-05-01 15:42:21 +0200

    Start website

---
+++ Web/Index.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kutay
+ * Date: 5/1/2017
+ * Time: 14:03
+ */
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="Main_Web_CSS">
+    <title>FiFa Toernooi</title>
+</head>
+<body>
+<header>
+
+    <div class="logo">
+
+        <img src="Ball.jpg" alt="a scorer ball">
+        <div class="slogan">
+
+            <p>FIFA</p>
+            <p>Radius College Toernooi</p>
+        </div>
+    </div>
+
+    <nav>
+
+        <a href="">Home</a>
+        <a href="">Intro</a>
+        <a href="">Groep 3</a>
+        <a href="">Toernooi</a>
+        <a href="">Teams</a>
+        <a href="../index.php">Login</a>
+    </nav>
+</header>
+
+<div class="container">
+    <div class="intro">
+        <h2>Welcome</h2>
+
+        <div class="description">
+
+            <h3>Lorem</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores ex nemo voluptates. Delectus dolorem facilis, ipsam laudantium nihil sapiente ullam vero voluptas. Asperiores dignissimos iure molestias nam nobis quam soluta.</p>
+        </div>
+
+        <div class="extra">
+
+            <h3>lorem</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam aperiam cupiditate debitis dignissimos dolor doloremque doloribus, eveniet, iusto nostrum perferendis possimus quibusdam repudiandae saepe totam unde vitae! Explicabo, mollitia.</p>
+        </div>
+    </div>
+
+    <div class="Groep">
+        <h2>Lorem</h2>
+
+        <div class="information">
+
+            <h3>lorem</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis cumque dolorum minus nostrum quisquam. Ab error facilis repellat vitae voluptatum! Ducimus explicabo impedit ipsum nemo qui! Adipisci asperiores tempore voluptates.</p>
+        </div>
+
+        <div class="Enginers">
+
+            <h2>The Brains Behind</h2>
+            
+            <div class="member">
+
+                <h4>Name</h4>
+                <img src="Placeholder_Profile_Pic.png" alt="a profile picture of one of the creators">
+
+                <div class="describe_Member">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias corporis dolorem expedita fugit nam nisi reiciendis totam. Dolorum et laborum quos temporibus? Impedit labore molestiae nihil optio praesentium, quasi similique!</p>
+                </div>
+            </div>
+
+            <div class="member">
+
+                <h4>Name</h4>
+                <img src="Placeholder_Profile_Pic.png" alt="a profile picture of one of the creators">
+
+                <div class="describe_Member">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias corporis dolorem expedita fugit nam nisi reiciendis totam. Dolorum et laborum quos temporibus? Impedit labore molestiae nihil optio praesentium, quasi similique!</p>
+                </div>
+            </div>
+
+            <div class="member">
+
+                <h4>Name</h4>
+                <img src="Placeholder_Profile_Pic.png" alt="a profile picture of one of the creators">
+
+                <div class="describe_Member">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias corporis dolorem expedita fugit nam nisi reiciendis totam. Dolorum et laborum quos temporibus? Impedit labore molestiae nihil optio praesentium, quasi similique!</p>
+                </div>
+            </div>
+
+            <div class="member">
+
+                <h4>Name</h4>
+                <img src="Placeholder_Profile_Pic.png" alt="a profile picture of one of the creators">
+
+                <div class="describe_Member">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias corporis dolorem expedita fugit nam nisi reiciendis totam. Dolorum et laborum quos temporibus? Impedit labore molestiae nihil optio praesentium, quasi similique!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="Tournament">
+
+        <h2>The Big Tournement</h2>
+
+        <div class="description">
+
+        </div>
+
+        <div class="rules">
+
+        </div>
+
+        <div class="Play">
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
