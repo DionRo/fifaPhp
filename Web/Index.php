8efcfe3f80f2414e034d223e8a7a722a2fded15d
Kutay Yalçinkaya <kutay_yal@outlook.com>
2017-05-08 16:02:46 +0200

    website 1.2.5

--- Web/Index.php
+++ Web/Index.php
@@ -11,20 +11,18 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script type="text/javascript" src="Scripts/scroll.js"></script>
-    <script type="text/javascript" !src="Scripts/SlideShow.js"></script>
     <link rel="stylesheet" href="Style/Main_Web_CSS.css">
-    <title>FiFa Toernooi</title>
+    <title>FiFa Tournament</title>
 </head>
 <body>
 <header id="Top-Header">
 
     <div class="main-head" id="main-head">
         <div class="logo">
-            <h1 ">FIFA Radius College Toernooi</h1>
+            <h1 ">FIFA Radius College Tournament</h1>
         </div>
 
         <nav>
@@ -49,40 +47,31 @@
             <div class="description">
 
                 <h3>Description</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores ex nemo voluptates. Delectus dolorem facilis, ipsam laudantium nihil sapiente ullam vero voluptas. Asperiores dignissimos iure molestias nam nobis quam soluta.</p>
+                <p>World's First Radius College FiFa Tournament</p>
+                <p>The FiFa tournament is a competition of students de parted in 14 to 16 teams that will compete each other to win the Big prize.</p>
+                <p>The players of the tournament are forming a team of 4 to 5 players. one keeper and 3~4 player on the field.</p>
+                &nbsp;
+                <p>There is also a betting place. If you interested you can bet on you're favorite Team. you can make profit of it so try you luck and who ones.</p>
+                <p>The bedding role is also a foundation for a charity for research of illnesses that need proper founding to make treatments for those terrible diseases.</p>
+                &nbsp;
+                <p>We hope that you ride along of this Tournament. Even if you are in for the game or for the joy of a bedding game.</p>
+                <p>We hope we see at the Game.</p>
+                <p>Radius College</p>
             </div>
 
             <div class="extra">
 
                 <h3>Extra</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam aperiam cupiditate debitis dignissimos dolor doloremque doloribus, eveniet, iusto nostrum perferendis possimus quibusdam repudiandae saepe totam unde vitae! Explicabo, mollitia.</p>
+                <p>Time's, Date's Place's and all what looks around.</p>
+                <ul>
+                    <li>Date:&emsp;&emsp;&emsp;&emsp;&emsp;22 - 06 - 2017</li>
+                    <li>Time:&emsp;&emsp;&emsp;&emsp;&emsp;10:00 ~ 16:00</li>
+                    <li>Game:&emsp;&emsp;&emsp;&emsp;&emsp;20 Min</li>
+                    <li>Minimum Player:&emsp;4 Members</li>
+                    <li>Maximim Player:&emsp;5 Members (Where 1 is reserve)</li>
+                </ul>
             </div>
         </div>
-
-        <h2>Some Picture</h2>
-
-        <div class="w3-content w3-section" style="max-width:500px" style="max-height: 300px">
-            <img class="mySlides" src="Img/Slideshow/slide_01.jpg" style="width:100%">
-            <img class="mySlides" src="Img/Slideshow/slide_02.jpg" style="width:100%">
-            <img class="mySlides" src="Img/Slideshow/slide_03.jpg" style="width:100%">
-        </div>
-
-        <script !src="">
-            var myIndex = 0;
-            carousel();
-
-            function carousel() {
-                var i;
-                var x = document.getElementsByClassName("mySlides");
-                for (i = 0; i < x.length; i++) {
-                    x[i].style.display = "none";
-                }
-                myIndex++;
-                if (myIndex > x.length) {myIndex = 1}
-                x[myIndex-1].style.display = "block";
-                setTimeout(carousel, 2000); // Change image every 2 seconds
-            }
-        </script>
     </div>
 
     <div class="Group" id="Group">
@@ -91,7 +80,11 @@
         <div class="information">
 
             <h3>Who we are</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis cumque dolorum minus nostrum quisquam. Ab error facilis repellat vitae voluptatum! Ducimus explicabo impedit ipsum nemo qui! Adipisci asperiores tempore voluptates.</p>
+            <p>We are Group 3 Company. whe are a 3 man company that are specialized in creating web application's an software application's.</p>
+            <p>When we here asked to take this project we where pleased that we where the first person to call. When we took on the job we begin te think how we going to make this so grade to rise a good amount of founding for the charity</p>
+            &nbsp;
+            <p>The design did we took of the world problems that we all have and can have, weird right if it isn't effecting us it will affect our friends an family around us.</p>
+            <p>We thought how we going to make a amazing application's to this project.</p>
         </div>
 
         <div class="Engineers">
