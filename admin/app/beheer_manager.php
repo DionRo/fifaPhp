be8937e0e42cbfc50cafd3d9972e968a307121bc
Lex Krooswijk <d229297@edu.rocwb.nl>
2017-06-07 14:04:41 +0200

    Auto stash before merge of "master" and "origin/master"

--- admin/app/beheer_manager.php
+++ admin/app/beheer_manager.php
@@ -13,9 +13,9 @@ if(isset($_POST["export-matches"])){
     header('Content-Type: text/csv; charset=utf-8');
     header('Content-Disposition: attachment; filename=matches.csv');
     $output = fopen("php://output", "w");
-    fputcsv($output, array('Game_id', 'HomeTeam', 'AwayTeam', 'HomeTeamScore', 'AwayTeamScore'));
+    fputcsv($output, array('Game_id', 'HomeTeam', 'AwayTeam', 'HomeTeamScore', 'AwayTeamScore', 'isPlayed'));
 
-    $query = 'SELECT `id`,`team_id_a`,`team_id_b`,`score_team_a`,`score_team_b` FROM tbl_matches';
+    $query = 'SELECT `id`,`team_id_a`,`team_id_b`,`score_team_a`,`score_team_b`, `isPlayed` FROM tbl_matches';
     $stmt = $db_conn->prepare($query);
     $stmt->execute();
 
